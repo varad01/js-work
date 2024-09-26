c33b9341ede47c810572e053c2fe159f7960abd5
varad01 <96991843+varad01@users.noreply.github.com>
2024-09-26 10:28:33 +0000

    variable done

---
+++ 01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1646
+let accountEmail = "varad@gmail.com" //variable
+var accountPassward = "123"    //variable
+//Prefet not to use var 
+// Because of issue in block code and function code
+
+accountCity = "Pune"
+let accountState;
+// accountId = 2 // we cannot change the constat value , Not allow
+ 
+
+accountEmail = "varad@123.com"
+accountPassward = "45645"
+accountCity = "pune"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountState])
