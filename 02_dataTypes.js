db7967b69f316283af35cb56c2a1b27951f57270
varad01 <96991843+varad01@users.noreply.github.com>
2024-09-26 10:47:44 +0000

    commit

---
+++ 02_dataTypes.js
@@ -0,0 +1,35 @@
+ "use strict"; // treat all JS code as newer version
+
+//  alert (3+3) // we are using nodeJS, not browser
+
+console.log(3+
+    3);    //code readability shoud be high
+
+console.log("varad")
+
+
+let name= "varad"
+let age = 22;
+let isLoggedIn = false
+
+
+//number => to power 53
+// bignit
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined =>    //let state;
+// symbol =>unique
+
+
+// object 
+
+console.log(typeof "varad")
+console.log(typeof true)
+console.log(typeof null)  //output-> null is object 
+console.log(typeof undefined)
+
+
+
+
+
